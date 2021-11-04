208d62c56ea456bd1b52137e713eb823cc1eacb3
Rajkumar <solomonrajkumar.as@gmail.com>
2021-11-04 15:44:32 +0530

    Raj | Updates snippet for checking for number and its double exists using set(performant version)

--- src/main/kotlin/arrrays/number-and-double-exist.kt
+++ src/main/kotlin/arrrays/number-and-double-exist.kt
@@ -3,26 +3,21 @@ package arrrays
 fun main() {
 //    val arr = arrayOf(-2,0,10,-19,4,6,-8)
 //    val arr = arrayOf(0,0)
-    val arr = arrayOf(-20,8,-6,-14,0,-19,14,4,-6)
+//    val arr = arrayOf(-20,8,-6,-14,0,-19,14,4,-6)
 //    val arr = arrayOf(-2,0,10,-19,4,6,-8)
+//    val arr = arrayOf(10,2,5,3)
+//    val arr = arrayOf(7,1,14,11)
+    val arr = arrayOf(3,1,7,11)
     val set = mutableSetOf<Int>()
-    var countOfZeroes = 0
-    for (i in arr.indices) {
-        if (arr[i] == 0) {
-            countOfZeroes++
-        } else if (arr[i] % 2 == 0) {
-            set.add(arr[i])
-        }
-    }
     var valueFound = false
     for (i in arr.indices) {
-        if(set.contains(arr[i] * 2)) {
+        if (set.contains(arr[i] * 2) || ( arr[i] % 2 == 0 && set.contains(arr[i] / 2))) {
             valueFound = true
             break
         }
-
+        set.add(arr[i])
     }
-    if (valueFound || countOfZeroes > 1) {
+    if (valueFound) {
         println("true")
     } else {
         println("false")
