54bc9b8006c4ce7e8e404e899c7cd59c8385b712
Rajkumar <solomonrajkumar.as@gmail.com>
2021-11-06 23:49:17 +0530

    Raj | Updates snippet for mountain array(enhanced)

--- src/main/kotlin/arrrays/mountain-array.kt
+++ src/main/kotlin/arrrays/mountain-array.kt
@@ -1,33 +1,30 @@
 package arrrays
 
 fun main() {
-//    var arr = arrayOf(2,1)
+    println(validateMountainArray())
+}
+
+fun validateMountainArray(): Boolean {
+        var arr = arrayOf(2,1)
 //    var arr = arrayOf(3,5,5)
-    var arr = arrayOf(0,3,2,1)
+//    var arr = arrayOf(0,3,2,1)
 //    var arr = arrayOf(1,2,3,4,5)
 //    var arr = arrayOf(6,5,4,3,2,1)
-    var isIncreasing = true
-    var isDecreasing = false
-    var isValid = true
+
     if (arr.size < 3) {
-        println("false")
-    } else {
-        if (arr[0] >= arr[1]) {
-            isValid = false
-        }  else{
-            for (i in 0 until arr.size - 1) {
-                if (arr[i] == arr[i+1]) {
-                    isValid = false
-                    break
-                } else if (arr[i] > arr[i+1]) {
-                    isDecreasing = true
-                    isIncreasing = false
-                } else if (arr[i] <= arr[i+1] && isDecreasing) {
-                    isValid = false
-                    break
-                }
-            }
-        }
-        println(isValid && !isIncreasing && isDecreasing)
+        return false
     }
+
+    var i = 0
+    var lastIndex = arr.size - 1
+    while (i < lastIndex && arr[i] < arr[i+1]) {
+        i++
+    }
+    if (i == 0 || i == lastIndex) {
+        return false
+    }
+    while (i < lastIndex && arr[i] > arr[i+1]) {
+        i++
+    }
+    return (i == lastIndex)
 }
